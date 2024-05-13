349a1d319487eccd771af0dec539c87e70a6e920
Marius Iota <146527216+iotamarius@users.noreply.github.com>
2024-05-13 19:14:05 +0300

    Update MealCardList.jsx

--- src/components/MealCardList.jsx
+++ src/components/MealCardList.jsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { MealCard } from "./MealCard";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
